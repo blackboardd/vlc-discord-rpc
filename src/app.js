de16a10a63d8a2103b6cd6a3dc82b74e59a509e2
Brighten Tompkins <brightenqtompkins@gmail.com>
2022-09-03 23:48:14 -0500

    :fire: (app.js) removed console log

--- src/app.js
+++ src/app.js
@@ -39,7 +39,6 @@ if (!(config.rpc.detached || process.argv.includes('detached'))) {
   const command = config.vlcPath || platformDefaults[process.platform] || 'vlc';
   const host = config.vlc.address.split(':')[1].replace('//', '');
   const port = config.vlc.address.split(':')[2];
-  console.log({ host, port });
   const child = spawn(command, ['--extraintf', 'http', '--http-host', host, '--http-password', config.vlc.password, '--http-port', port]);
   child.on('exit', () => {
   	console.log("VLC closed; Exiting.");
